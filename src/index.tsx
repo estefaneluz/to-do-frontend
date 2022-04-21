7efaa81b957707061b6252765b10915478dc1ed9
estefane <estefanecortes@gmail.com>
2022-04-21 18:22:25 -0300

    STYLE: created global styles file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Routes from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Routes from './routes';
+import Styles from './styles';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
+    <Styles />
     <Routes />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
