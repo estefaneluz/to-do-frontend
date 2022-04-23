d403e5189706740ea87469192fa5de42b4c03129
estefane <estefanecortes@gmail.com>
2022-04-23 00:18:23 -0300

    STYLE: set theme color

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -6,4 +6,12 @@ export default createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
+
+    :root {
+      --white-color: #E5E5E5;
+      --light-color: #F4FCFF;
+      --primary-color: #007E9B;
+      --secondary-color: #3092AD;
+      --text-color: #064556;
+    }
 `;
